02f01743e17592ca149ba72de80886ab374d645c
Rubens Luca <Rubensluca12@gmail.com>
2025-07-14 18:33:18 -0300

    feat:criar as rotas do missoes controler com mock

--- src/main/java/dev/Java10x/CadastroDeNinjas/Missões/MissoesController.java
+++ src/main/java/dev/Java10x/CadastroDeNinjas/Missões/MissoesController.java
@@ -1,17 +1,28 @@
 package dev.Java10x.CadastroDeNinjas.Missões;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("missoes")
 public class MissoesController {
-    @RestController
-    @RequestMapping
-    public static class missoesController{
-        @GetMapping("/missoes")
-        public String missoes(){
-            return "rota de missões";
-        }
-    }
-
+    // POST-mandar uma requisição para criar uma missao
+@PostMapping("/criar")
+    public String criarMissao(){
+    return "missao criada com sucesso";
 }
+// PUT-manda uma requisição para alterar as missoes
+@PutMapping("/alterar")
+    public String alterarMisaao(){
+    return "missao alterada";
+}
+// DELETE-manda uma requisição para deletar missoes
+@DeleteMapping("/deletar")
+    public String deletarMissao(){
+return "missao deletada";
+}
+//GET-manda uma requisição para listar as missoes
+@GetMapping("/listar")
+    public String listarMissoes(){
+    return "misssoes listadas";
+}
+    }
